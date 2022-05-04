d112fb2e5545261ba56c0e0870ce3367ea9dc1e6
Henrique <henriqueamaralhp@gmail.com>
2022-05-04 19:49:51 -0300

    update files

--- exercíciosJs/EX_12/index.js
+++ exercíciosJs/EX_12/index.js
@@ -1,11 +1,26 @@
-ShowPrompt = () =>{
-    clock = 300
-    document.getElementById("result").innerHTML = ""
-    for(let i = 0; i < 10; i++){
-        setTimeout(()=>{
-            document.getElementById("result").innerHTML += `${i+1} `
-        }, clock)
+var i = 1
+const letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
 
-        clock += 200    
+
+function ShowPrompt(){
+    setInterval(function() {
+        let color = getRandomNumber()
+        const div = document.createElement("div")
+        div.style.color = color
+        div.style.transition = "0.5s"
+        div.addEventListener("mouseover", () => { 
+            div.style.fontSize = "50px"
+        })
+        div.innerHTML = `${i} `
+        document.getElementById("result").appendChild(div)
+        i++
+    }, 30)
+}
+
+function getRandomNumber(){
+    let color = "#"
+    for(let j = 0; j < 6; j++){
+        color += letters[Math.floor(Math.random() * 16)] 
     }
+    return color
 }
